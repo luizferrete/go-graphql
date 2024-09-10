10c2043929111db615425da9779e5b994fddd145
Luiz Amaral <luiz.amaral@zeev.it>
2024-09-09 23:12:24 -0300

    first commit

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -12,12 +12,12 @@ import (
 // CreateCategory is the resolver for the createCategory field.
 func (r *mutationResolver) CreateCategory(ctx context.Context, input model.NewCategory) (*model.Category, error) {
 	category, err := r.CategoryDB.Create(input.Name, *input.Description)
-	if err != nil{
+	if err != nil {
 		return nil, err
 	}
 	return &model.Category{
-		ID: category.ID,
-		Name: category.Name,
+		ID:          category.ID,
+		Name:        category.Name,
 		Description: &category.Description,
 	}, nil
 }
@@ -25,12 +25,12 @@ func (r *mutationResolver) CreateCategory(ctx context.Context, input model.NewCa
 // CreateCourse is the resolver for the createCourse field.
 func (r *mutationResolver) CreateCourse(ctx context.Context, input model.NewCourse) (*model.Course, error) {
 	course, err := r.CourseDB.Create(input.Name, *input.Description, input.CategoryID)
-	if err != nil{
+	if err != nil {
 		return nil, err
 	}
 	return &model.Course{
-		ID: course.ID,
-		Name: course.Name,
+		ID:          course.ID,
+		Name:        course.Name,
 		Description: &course.Description,
 	}, nil
 }
@@ -45,8 +45,8 @@ func (r *queryResolver) Categories(ctx context.Context) ([]*model.Category, erro
 	var categoriesModel []*model.Category
 	for _, category := range categories {
 		categoriesModel = append(categoriesModel, &model.Category{
-			ID: category.ID,
-			Name: category.Name,
+			ID:          category.ID,
+			Name:        category.Name,
 			Description: &category.Description,
 		})
 	}
@@ -63,8 +63,8 @@ func (r *queryResolver) Courses(ctx context.Context) ([]*model.Course, error) {
 	var coursesModel []*model.Course
 	for _, course := range courses {
 		coursesModel = append(coursesModel, &model.Course{
-			ID: course.ID,
-			Name: course.Name,
+			ID:          course.ID,
+			Name:        course.Name,
 			Description: &course.Description,
 		})
 	}
